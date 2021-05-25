e64001dbad2cdb5188ffa0e3069ae0fe1dab113f
shadow1399 <yashvaish1399@gmail.com>
2021-05-25 23:53:01 +0530

    Delete Functionality on Post and Comments

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,5 +1,5 @@
 const Post = require("../models/postSchema");
-
+const Comment = require("../models/commentSchema");
 module.exports.create = function (req, res) {
     Post.create({
         content: req.body.content,
@@ -13,4 +13,30 @@ module.exports.create = function (req, res) {
         // console.log("***", newPost);
         return res.redirect("back");
     })
+}
+
+module.exports.delete = function (req, res) {
+
+    // console.log(req.params.id);
+    Post.findById(req.params.id, function (err, post) {
+        if (err) {
+            console.log("Error in deleting post", err);
+            return;
+        }
+        if (post.user == req.user.id) {
+            post.remove();
+            // console.log("Successfully Deleted");
+            Comment.deleteMany({ post: { $in: req.params.id } }, function (err) {
+                if (err) {
+                    console.log("Error in deleting comments", err);
+                    return;
+                }
+                // console.log("Comments Deleted");
+            })
+        }
+        // post.remove();
+
+
+    });
+    return res.redirect("back");
 }
--- routes/posts.js
+++ routes/posts.js
@@ -6,6 +6,7 @@ const post_controller = require("../controllers/post_controller");
 
 
 router.post("/create", passport.checkAuthentication, post_controller.create);
+router.get("/delete/:id", passport.checkAuthentication, post_controller.delete);
 
 
 module.exports = router;
